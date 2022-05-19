726b1b90d24f5533a2073e9cd30980c4dfb0bb4e
Paula Sorolla <55831646+paula-sorolla@users.noreply.github.com>
2022-05-19 15:21:45 +0200

    Added requirements

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# multilabel-classification
-Multilabel classification using BERT embeddings
+# Multilabel classification using BERT embeddings
+
+This is the main repository for the Internship Thesis developed by Paula Sorolla.
+The project is intended to provide improvements and new approaches to the document-level classification component in the Canary pipeline.
+
+The goal and all previous work made at Elsevier are summarized in the following Confluence page: https://confluence.elsevier.com/display/ContentAssets/Document-Level+Classification.
+
+## Structure
+Below is the structure of the repo with a brief description.
+```
+repo/
+  notebooks/
+    BERT_classifier.ipynb - Includes the working notebook where the first experiments have been setup and run
+  src/
+    Classifier.py - Includes the Classifier network model that is being trained in the experimental phase
+    Dataset.py 
+  
+```
+
+## Setup
--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -6,9 +6,40 @@ nltk.download('punkt')
 import re
 import pandas as pd
 
+import torch
 from torch.utils.data import DataLoader
 
-from src.Dataset import Dataset
+class Dataset:
+    def __init__(self, texts, labels, tokenizer, max_len):
+        self.texts = texts
+        self.labels = labels
+
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+    
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, index):
+        text = self.texts[index]
+        label = self.labels[index]
+
+        inputs = self.tokenizer.__call__(text,
+                                        None,
+                                        add_special_tokens=True,
+                                        max_length=self.max_len,
+                                        padding="max_length",
+                                        truncation=True,
+                                        )
+        ids = inputs["input_ids"]
+        mask = inputs["attention_mask"]
+
+        return {
+            "ids": torch.tensor(ids, dtype=torch.long),
+            "mask": torch.tensor(mask, dtype=torch.long),
+            "labels": torch.tensor(label, dtype=torch.long)
+        }
+
 
 class Preprocessing(object):
 
@@ -110,6 +141,7 @@ class Preprocessing(object):
         return train, test, val
 
     def build_dataset(self, tokenizer, tokenizer_max_len, truncate):
+        
         '''
         Tokenize and map the training and validation sets
         '''
@@ -146,3 +178,34 @@ class Preprocessing(object):
         
         except:
             return test
+
+class Dataset:
+    def __init__(self, texts, labels, tokenizer, max_len):
+        self.texts = texts
+        self.labels = labels
+
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+    
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, index):
+        text = self.texts[index]
+        label = self.labels[index]
+
+        inputs = self.tokenizer.__call__(text,
+                                        None,
+                                        add_special_tokens=True,
+                                        max_length=self.max_len,
+                                        padding="max_length",
+                                        truncation=True,
+                                        )
+        ids = inputs["input_ids"]
+        mask = inputs["attention_mask"]
+
+        return {
+            "ids": torch.tensor(ids, dtype=torch.long),
+            "mask": torch.tensor(mask, dtype=torch.long),
+            "labels": torch.tensor(label, dtype=torch.long)
+        }
